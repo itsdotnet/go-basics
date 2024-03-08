c05e3da01517d27a8dc876f0faaba77b8d1a2e0e
Iskandar Kodirov <eiscocyber@gmail.com>
2024-03-08 09:43:09 +0500

    firs and second lessons by myself

---
+++ bir/first.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/*So niggas we started GO from here*/
+	
+	fmt.Println("Iskandar Qodirov") // its better than hello world
+
+}
---
+++ second/second.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nigga = "Iskandar Qodirov"
+
+	var age int = 18
+
+	fmt.Println("FIO: " + nigga + "\nMy Age: ", age)
+
+}
